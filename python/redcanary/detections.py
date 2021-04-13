0dbf1f73b2a5107a353682c7d66bcd0f7d3785ec
Matt Hastings <>
2021-04-13 10:04:15 -0400

    Splitting out detector class

--- python/redcanary/detections.py
+++ python/redcanary/detections.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 from .rest import RestClient
+from .detectors import Detector
 
 class Detections(RestClient):
     """
@@ -84,51 +85,6 @@ class Detections(RestClient):
         api_path = ('/').join(full_url.split('/')[5:])
         detection.detectors = [Detector(i) for i in self._get_all(api_path)]
         return detection
-
-    
-class Detector(object):
-    """
-    Object class for a red canary detector
-    """
-
-    def __init__(self, detector: dict):
-        """
-        Parameters
-        --------
-        data item : json
-        """
-        self._detector = detector
-        self._attributes = detector.get('attributes')
-
-    @property
-    def id(self) -> int:
-        """Unique identifier of the detector"""
-        return self._detector.get('id')
-    
-    @property
-    def name(self) -> str:
-        """Name of the Red Canary detector"""
-        return self._attributes.get('name')
-    
-    @property
-    def description(self) -> str:
-        """Description of the activity the detector identifies in Markdown format"""
-        return self._attributes.get('description')
-    
-    @property
-    def contributing_intelligence(self) -> str:
-        """The type of adversary intelligence supporting this detector."""
-        return self._attributes.get('contributing_intelligence')
-
-    @property
-    def attack_technique_identifiers(self) -> list:
-        """The specific ATT&CK Techniques the detector maps to"""
-        return self._attributes.get('attack_technique_identifiers', [])
-
-    @property
-    def relationships(self) -> dict:
-        """Resources related to this object"""
-        return self._detector.get('relationships')
 
 class Detection(object):
     """
